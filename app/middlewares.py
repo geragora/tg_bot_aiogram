b90f869f59ff2cf8468faaee05767e3101b5c318
Egor Gorbunov <egor-gorbunov-2017@mail.ru>
2025-02-02 17:15:03 +0300

    логи

--- app/middlewares.py
+++ app/middlewares.py
@@ -1,17 +1,7 @@
-import logging
 from aiogram import BaseMiddleware
 from aiogram.types import Message
 
-# Настройка логирования
-logging.basicConfig(
-    filename="bot_logs.log",  # Имя файла для логов
-    level=logging.INFO,  # Уровень логирования
-    format="%(asctime)s - %(levelname)s - %(message)s",  # Формат сообщений
-)
-
-
 class LoggingMiddleware(BaseMiddleware):
     async def __call__(self, handler, event: Message, data: dict):
-        # Логирование полученного сообщения
-        logging.info(f"Получено сообщение: {event.text}")
+        print(f"Получено сообщение: {event.text}")
         return await handler(event, data)
